e2899a9b44e66b95f0808a6ef224f1c03aaebe99
ralphfrncisco <ralphfrncisco@gmail.com>
2025-12-09 03:18:18 +0800

    mispelling of received. (antok na ko boss)

---
+++ src/pages/Purchasing/ReceivedItemsTableHeader.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ReceivedItemsTableHeader() {
+  return (
+    <div>Untitled-1</div>
+  )
+}
+
+export default ReceivedItemsTableHeader;
--- src/pages/Purchasing/RecievedItemsTableHeader.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function RecievedItemsTableHeader() {
-  return (
-    <div>Untitled-1</div>
-  )
-}
-
-export default RecievedItemsTableHeader;
